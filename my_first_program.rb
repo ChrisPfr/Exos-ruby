d2376443a5a0bacc32ac245b67e8e2773a8ef4f5
ChrisPfeiffer <chris.cp147@gmail.com>
2022-07-04 15:28:44 +0200

    my_first_program.rb

---
+++ my_first_program.rb
@@ -0,0 +1,16 @@
+
+
+puts "Bonjour, c'est quoi ton blase ?"
+print "> "
+userfirst_name = gets.chomp
+
+print "> "
+userlast_name = gets.chomp
+
+
+
+
+puts "Salut #{ userfirst_name + userlast_name} !" 
+
+
+
